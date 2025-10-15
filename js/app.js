f9503fb016f7a62a736d23014810a0f4537bc16b
haftamudesta <desta451616@hotmail.com>
2025-10-15 11:04:25 -0800

    Add ui

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 import { sounds,defaultPresets } from "./soundData.js";
 import { SoundManager } from "./soundManager.js";
+import {UI} from "./ui.js"
 
 class AmbientMixer {
   constructor() {
     this.soundManager = new SoundManager();
-    this.ui = null;
+    this.ui = new UI();
     this.presetManager = null;
     this.timer = null;
     this.currentSoundState = {};
     this.isInitialized = false;
   }
   async init() {
+    this.ui.init();
+    this.ui.renderSoundCards(sounds)
+    this.setupEventListeners()
     try {
       this.loadAllSounds();
       this.isInitialized = true;
@@ -27,6 +31,52 @@ class AmbientMixer {
       }
     });
   }
+
+  setupEventListeners() {
+    document.addEventListener('click', async (e) => {
+      if (e.target.closest('.play-btn')) {
+        const soundId = e.target.closest('.play-btn').dataset.sound;
+        await this.toggleSound(soundId);
+      }
+    })
+}
+
+
+async toggleSound(soundId) {
+    const audio = this.soundManager.audioElements.get(soundId);
+
+    if (!audio) {
+      console.error(`Sound ${soundId} not found`);
+      return false;
+    }
+
+    if (audio.paused) {
+      
+      const card = document.querySelector(`[data-sound="${soundId}"]`);
+      const slider = card.querySelector('.volume-slider');
+      let volume = parseInt(slider.value);
+
+      if (volume === 0) {
+        volume = 50;
+        this.ui.updateVolumeDisplay(soundId, volume);
+      }
+
+      this.currentSoundState[soundId] = volume;
+
+      this.soundManager.setVolume(soundId, volume);
+      await this.soundManager.playSound(soundId);
+      this.ui.updateSoundPlayButton(soundId, true);
+    } else {
+      this.soundManager.pauseSound(soundId);
+      this.currentSoundState[soundId] = 0;
+      this.ui.updateSoundPlayButton(soundId, false);
+
+      this.currentSoundState[soundId] = 0;
+    }
+
+    this.updateMainPlayButtonState();
+  }
+
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
