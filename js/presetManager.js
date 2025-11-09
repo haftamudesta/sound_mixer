1d98cca33b2049668de9f935c51bbec6d35141aa
Haftamu Desta <haftamudesta>
2025-11-08 21:59:03 -0800

    Load custom preset

--- js/presetManager.js
+++ js/presetManager.js
@@ -2,6 +2,10 @@ export class PresetManager {
     constructor() {
         this.customPresets = this.loadCustomPresets();
     }
+    loadCustomPresets() {
+        const stored = localStorage.getItem('ambientMixerPresets');
+        return stored ? JSON.parse(stored) : {};
+    }
     loadPreset(presetId) {
         return this.customPresets[presetId] || null;
     }
