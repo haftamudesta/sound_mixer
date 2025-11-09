a96ace992d551863459cafaf1445f4b727eb4897
Haftamu Desta <haftamudesta>
2025-11-08 22:05:46 -0800

    Save current mix as preset

--- js/presetManager.js
+++ js/presetManager.js
@@ -15,4 +15,26 @@ export class PresetManager {
     loadPreset(presetId) {
         return this.customPresets[presetId] || null;
     }
+
+    savePreset(name, soundStates) {
+        const presetId = `custom-${Date.now()}`;
+
+        const preset = {
+            name,
+            sounds: {},
+        };
+
+        for (const [soundId, volume] of Object.entries(soundStates)) {
+            if (volume > 0) {
+                preset.sounds[soundId] = volume;
+            }
+        }
+
+        this.customPresets[presetId] = preset;
+        this.saveCustomPresets();
+
+        return presetId;
+    }
+
+
 }
