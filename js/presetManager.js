028455db90a303a3d600f152e0745c881ed4425f
Haftamu Desta <haftamudesta>
2025-11-08 22:00:03 -0800

    Save custom pre set

--- js/presetManager.js
+++ js/presetManager.js
@@ -6,6 +6,12 @@ export class PresetManager {
         const stored = localStorage.getItem('ambientMixerPresets');
         return stored ? JSON.parse(stored) : {};
     }
+    saveCustomPresets() {
+        localStorage.setItem(
+            'ambientMixerPresets',
+            JSON.stringify(this.customPresets)
+        );
+    }
     loadPreset(presetId) {
         return this.customPresets[presetId] || null;
     }
